ecba4f6a6c9e45faecf24e0c93e379167173d0fe
Muhammad Huzaifa <muhammad.huzaifa1620@gmail.com>
2021-05-14 11:34:10 +0500

    Flutter My name app

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(muhammadhuzaifa());
+}
+
+
+class muhammadhuzaifa extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: 
+        Scaffold(body: Text("Muhammad Huzaifa Shafique") ,)
+      ,
+      
+    );
+  }
+}
+
+
+
+  
